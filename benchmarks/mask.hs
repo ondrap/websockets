b5822840ae47eec011b261bf276c7c06eed3edb3
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2017-05-05 08:17:48 +0200

    Added more unaligned benchmarks.

--- benchmarks/mask.hs
+++ benchmarks/mask.hs
@@ -13,7 +13,9 @@ import qualified Data.ByteString.Lazy as BL
 setupEnv = do
     let kilo = BL.replicate 1024 37
         mega = BL.replicate (1024 * 1024) 37
-    return (kilo, mega)
+        megaU = BL.fromChunks [B.drop 1 (B.replicate (1024 * 1024) 37)]
+        megaS = BL.fromChunks [B.replicate (1024 * 1024) 37]
+    return (kilo, mega, megaU, megaS)
 
 maskPayload' :: Mask -> BL.ByteString -> BL.ByteString
 maskPayload' Nothing     = id
@@ -23,7 +25,7 @@ maskPayload' (Just mask) = snd . BL.mapAccumL f (cycle $ B.unpack mask)
     f (m:ms) !c = (ms, m `xor` c)
 
 main = defaultMain [
-    env setupEnv $ \ ~(kilo, mega) -> bgroup "main"
+    env setupEnv $ \ ~(kilo, mega, megaU, megaS) -> bgroup "main"
         [ bgroup "kilobyte payload"
             [ bgroup "zero_mask"
                 [ bench "current" $ nf (maskPayload (Just "\x00\x00\x00\x00")) kilo
@@ -53,9 +55,11 @@ main = defaultMain [
                 ]
             ,  bgroup "full_mask"
                 [ bench "current" $ nf (maskPayload (Just "\xFF\xFF\xFF\xFF")) mega
-                , bench "current-unaligned" $ nf (maskPayload (Just "\xFF\xFF\xFF\xFF")) (BL.drop 1 mega)
+                , bench "current-unaligned" $ nf (maskPayload (Just "\xFF\xFF\xFF\xFF")) megaU
+                , bench "current-aligned" $ nf (maskPayload (Just "\xFF\xFF\xFF\xFF")) megaS
                 , bench "simple" $ nf (maskPayloadSimple (Just "\xFF\xFF\xFF\xFF")) mega
-                , bench "simple-unaligned" $ nf (maskPayloadSimple (Just "\xFF\xFF\xFF\xFF")) (BL.drop 1 mega)
+                , bench "simple-unaligned" $ nf (maskPayloadSimple (Just "\xFF\xFF\xFF\xFF")) megaU
+                , bench "simple-aligned" $ nf (maskPayloadSimple (Just "\xFF\xFF\xFF\xFF")) megaS
                 , bench "old" $ nf (maskPayload' (Just "\xFF\xFF\xFF\xFF")) mega
                 ]
             ,  bgroup "one_byte_mask"
